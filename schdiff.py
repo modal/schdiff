881fed071dcdef9525eb59f2b5ae85e3cc0545e4
Tim Dahlin <duhlynn@gmail.com>
2015-08-10 15:59:14 -0500

    Implement a faster diff using ImageMath

--- schdiff.py
+++ schdiff.py
@@ -3,15 +3,45 @@ schdiff.py takes two images and produces a differenced output
 
 Usage:
 schdiff.py old.jpg new.jpg diff.png
-
 """
-
 from PIL import Image, ImageMath
 import sys
+import time
 
 if len(sys.argv) != 4:
     print "schdiff.py old.jpg new.jpg diff.png"
     sys.exit()
+##############################################################################
+R, G, B = 0, 1, 2   #RGB Band indices
+invertband=lambda i : 255 - i
+##############################################################################
+def split_process_old(im=None):
+    "This forces red band to 0 and inverts green/blue bands"
+    src=im.split()
+    rb=src[R].point(lambda i: 0)
+    src[R].paste(rb, None, None)
+    gb=src[G].point(invertband)
+    src[G].paste(gb, None, None)
+    bb=src[B].point(invertband)
+    src[B].paste(bb, None, None)
+    return src
+
+def split_process_new(im=None):
+    "Set green band in image to maximum"
+    src=im.split()
+    gb=src[G].point(lambda i: 255)
+    src[G].paste(gb, None, None)
+    return src
+
+def xor_using_image_math(src0=None, src1=None):
+    start_time = time.time()
+    rTmp = ImageMath.eval("a^b", a=src0[R], b=src1[R]).convert('L')
+    gTmp = ImageMath.eval("a^b", a=src0[G], b=src1[G]).convert('L')
+    bTmp = ImageMath.eval("a^b", a=src0[B], b=src1[B]).convert('L')
+    merged = Image.merge("RGB", (rTmp, gTmp, bTmp))
+    print "Fast Method Time %8.3f" % (time.time() - start_time,)
+    return merged
+
 ##############################################################################
 om = Image.open(sys.argv[1])        #Old Image
 im = Image.open(sys.argv[2])        #Lastest Image
@@ -28,59 +58,46 @@ print sys.argv[2]
 print "Image Size:  %dx%d" % (width, height)
 print "Image Mode:  %s" % im.mode
 
-#Must issue load method for split method to work
+#Must issue load method for split methods to work
 pim = im.load()
 pom = om.load()
 
-R, G, B = 0, 1, 2   #RGB Band indices
-invertband=lambda i : 255 - i
+src0 = split_process_old(om)
+om = Image.merge(om.mode, src0)
+om.save("tmp_new.jpg")
+del om
+del pom
+src1 = split_process_new(im)
+im = Image.merge(im.mode, src1)
+im.save("tmp_old.jpg")
+del im
+del pim
 
-#src0=om.split()
-#rb=src0[R].point(lambda i: 0)
-#src0[R].paste(rb, None, None)
-#gb=src0[G].point(invertband)
-#src0[G].paste(gb, None, None)
-#bb=src0[B].point(invertband)
-#src0[B].paste(bb, None, None)
-#om = Image.merge(om.mode, src0)
-#om.save("tmpn.jpg")
-##del om              #Free Up Memory
-#
-#src1=im.split()
-#gb=src1[G].point(lambda i: 255)
-#src1[G].paste(gb, None, None)
-#im = Image.merge(im.mode, src1)
-#im.save("tmpo.jpg")
-##del im              #Free Up Memory
-
-#nm = ImageMath.eval("int(a^b)", a=im, b=om)
-#nm = ImageMath.eval("convert(a^b, 'L')", a=im, b=om)
-
-#rTmp = ImageMath.eval("a^b", a=src0[R], b=src1[R])
-#gTmp = ImageMath.eval("a^b", a=src0[G], b=src1[G])
-#bTmp = ImageMath.eval("a^b", a=src0[B], b=src1[B])
-#nm = Image.merge("RGB", (rTmp, gTmp, bTmp))
-
+nm = xor_using_image_math(src0, src1)
+tbn_size= (int(width/4), int(height/4))
+nm.thumbnail(tbn_size, Image.ANTIALIAS)
+nm.save(sys.argv[3], compress_level=9) #optimize=True)
 
 #Slow Method using direct pixel access
+#start_time = time.time()
 #ptm = tm.load()
-nm = Image.new("RGB", im.size)      #Difference Image
-tm = Image.new("RGB", im.size)
-pnm = nm.load()
-ptm = tm.load()
-for x in range(width):
-    for y in range(height):
-        r0, g0, b0 = pim[x,y]
-        r1, g1, b1 = pom[x,y]
-        g0 = 255
-
-        r1 = 0
-        g1 = 255 - g1
-        b1 = 255 - b1
-
-        pnm[x, y] = (r0 ^ r1, g0 ^ g1, b0 ^ b1)
-        #ptm[x, y] = (r1, g1, b1)
-        #pnm[x,y]= (r, 255, b) #Make green
-        #pnm[x,y]= (255, g, b) #Make red
-
-nm.save(sys.argv[3])
+#nm = Image.new("RGB", im.size)      #Difference Image
+#tm = Image.new("RGB", im.size)
+#pnm = nm.load()
+#ptm = tm.load()
+#for x in range(width):
+#    for y in range(height):
+#        r0, g0, b0 = pim[x,y]
+#        r1, g1, b1 = pom[x,y]
+#        g0 = 255
+#
+#        r1 = 0
+#        g1 = 255 - g1
+#        b1 = 255 - b1
+#
+#        pnm[x, y] = (r0 ^ r1, g0 ^ g1, b0 ^ b1)
+#        #ptm[x, y] = (r1, g1, b1)
+#        #pnm[x,y]= (r, 255, b) #Make green
+#        #pnm[x,y]= (255, g, b) #Make red
+#print time.time() - start_time
+#nm.save(sys.argv[3], optimize=1)
